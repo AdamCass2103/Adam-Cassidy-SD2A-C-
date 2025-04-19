623cc62f1111fbdd7da61786e8312e6fdf5d8ed8
Adam Cassidy <D00265095@student.dkit.ie>
2025-04-19 14:06:02 +0100

    Grid

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-//
-// Created by student on 07/04/2025.
-//
+#include <iostream>
+#include "Grid.hpp"
+#include "Ant.hpp"
+#include "Doodlebug.hpp"
 
-#include "main.h"
+int main() {
+    Grid grid;
+
+    // Place some bugs
+    grid.setCell(2, 3, new Ant(2, 3));
+    grid.setCell(5, 5, new Doodlebug(5, 5));
+
+    std::cout << "Welcome to A Bug's Life Simulation!\n";
+    grid.display();
+
+    // TODO: Menu and interaction logic next
+    return 0;
+}
