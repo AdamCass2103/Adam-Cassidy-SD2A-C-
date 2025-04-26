919797a81b917a15877d406ffea94de24f46cc79
Adam Cassidy <D00265095@student.dkit.ie>
2025-04-26 14:55:35 +0100

    updated bug

--- Adam_Cassidy_SD2A_C++/Bug.cpp
+++ Adam_Cassidy_SD2A_C++/Bug.cpp
@@ -1,9 +1,18 @@
 #include "Bug.h"
 
-Bug::Bug(int startX, int startY) : x(startX), y(startY), alive(true), breedCounter(0) {}
+Bug::Bug(int id, Position pos, Direction dir, int size)
+    : id(id), position(pos), direction(dir), size(size), alive(true), breedCounter(0) {
+    path.push_back(pos); // start path with initial position
+}
 
-int Bug::getX() const { return x; }
-int Bug::getY() const { return y; }
+int Bug::getId() const { return id; }
+Position Bug::getPosition() const { return position; }
+void Bug::setPosition(Position newPos) { position = newPos; }
 bool Bug::isAlive() const { return alive; }
 void Bug::setAlive(bool status) { alive = status; }
-void Bug::setPosition(int newX, int newY) { x = newX; y = newY; }
+int Bug::getSize() const { return size; }
+void Bug::grow(int amount) { size += amount; }
+void Bug::addToPath(Position pos) { path.push_back(pos); }
+std::list<Position> Bug::getPath() const { return path; }
+Direction Bug::getDirection() const { return direction; }
+void Bug::setDirection(Direction dir) { direction = dir; }
